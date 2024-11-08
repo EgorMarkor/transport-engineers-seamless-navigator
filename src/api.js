26d5b15495a2918416f517b5747a559ba3baba9d
SteeperMold <112201965+SteeperMold@users.noreply.github.com>
2024-11-08 23:48:38 +0400

    Test build v2

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://194.87.95.102/api",
   headers: {
     "Content-Type": "application/json",
   },
